ae2983f1dcc8653ce77da04752cbe86b47a291fb
fhery021 <fhery021@gmail.com>
2021-03-15 18:29:58 +0100

    resource.getFile() does not work when running on docker

--- src/main/java/com/example/simpledatawarehouse/csv/CSVReaderService.java
+++ src/main/java/com/example/simpledatawarehouse/csv/CSVReaderService.java
@@ -5,8 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 @Service
@@ -15,13 +14,36 @@ public class CSVReaderService {
     @Value("classpath:statistics.csv")
     private Resource res;
 
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+
     public List<Statistics> processCSV() throws IOException {
 
-        List<Statistics> statistics = new CsvToBeanBuilder(new FileReader(res.getFile()))
+        InputStream inputStream = res.getInputStream();
+
+        File file = new File("csv");
+        copyInputStreamToFile(inputStream, file);
+
+
+        List<Statistics> statistics = new CsvToBeanBuilder(new FileReader(file))
                 .withType(Statistics.class)
                 .build()
                 .parse();
 
         return statistics;
+    }
+
+    // FileReader does not work under docker
+    private static void copyInputStreamToFile(InputStream inputStream, File file)
+            throws IOException {
+
+        // append = false
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            int read;
+            byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
+
     }
 }
